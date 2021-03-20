070248dba0794aff1a841e19556c9d7b01619804
zoë <zoe@Zoes-MacBook-Pro.local>
2021-03-20 12:02:39 -0400

    name page works

--- assets/js/amimalsScript.js
+++
@@ -1,3 +0,0 @@
-var apiKey = "9OfuZfc6gZ4qMTj3UypnftP8bY9EJWLfTGQiK0vP3NGC3MniSj";
-var secret = "CoLGacoP8HgadyuzPs2XIWOFPDAWOtHWkYhWxtMx"
-// var apiURL = ""
--- assets/js/indexScript.js
+++ assets/js/indexScript.js
@@ -60,9 +60,29 @@ function fetchAnimals(url) {
 
 $("#animalResults").on("click", ".save-btn", function(){
   var animalInfo = animalArray[$(this).attr('data-pos')];
+
+  if (animalInfo.primary_photo_cropped) {
+    var srcImage = animalInfo.primary_photo_cropped.small
+  } else {
+    var srcImage = 'https://sanfrancisco.cbslocal.com/wp-content/uploads/sites/15116056/2011/10/pets.jpg?w=420'
+  }
+   var name = animalInfo.name;
+   var gender = animalInfo.gender;
+   var description = animalInfo.description;
+   var age = animalInfo.age;
+   var link = animalInfo.URL;
+  // this is gross because it's a materialize card
+  $("#favorite").append("<div class='col s12 m6'><div class='card'><div class='card-image'><img src="+ srcImage + "><span class='card-title'>" + name +"</span><a class='lock-btn btn-floating halfway-fab waves-effect waves-light red' data-pos=><i class='material-icons'>🔒</i></a></div><div class='card-content'><p>Gender: " + gender + "<br>" + age + "<br>" + description + "<br><a href='" + link + "'>Learn More</a></p></div></div></div></div>")
+
    favorites.push (animalInfo);
   window.localStorage.setItem("favorites", JSON.stringify(favorites));
+
 });
+
+$("#animalResults").on("click", ".lock-btn", function() {
+
+});
+
 
 function search(animaltype, city) {
   let testURL = 'https://api.petfinder.com/v2/animals?type=' + animaltype + '&location=' + city;
