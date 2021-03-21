bd374ffd767abf328a3f41b59fb2988a8ecdb11e
zchalk <you@example.com>
2021-03-21 15:34:29 -0400

    everything works and i have literally never made a mistake

--- assets/js/indexScript.js
+++ assets/js/indexScript.js
@@ -2,7 +2,7 @@
 var catButton = $("#cat");
 var dogButton = $("#dog");
 var favorites =  JSON.parse(localStorage.getItem("favorites")) || [];
-var theOne;
+var theOne = JSON.parse(localStorage.getItem("theOne")) || [];
 var animalArray;
 
 catButton.on("click", function () {search("cat", $("#zipCode").val())});
@@ -27,6 +27,10 @@ function getToken() {
       }
     });
 }
+function search(animaltype, city) {
+  let testURL = 'https://api.petfinder.com/v2/animals?type=' + animaltype + '&location=' + city;
+  fetchAnimals(testURL);
+  };
 // make sure you have a token before using this!
 function fetchAnimals(url) {
   fetch(url, {
@@ -50,8 +54,10 @@ function fetchAnimals(url) {
          var description = animalArray[i].description;
          var age = animalArray[i].age;
          var link = animalArray[i].URL;
+         var position = [i];
+         console.log(position);
         // this is gross because it's a materialize card
-        $("#animalResults").append("<div class='col s12 m6'><div class='card'><div class='card-image'><img src="+ srcImage + "><span class='card-title'>" + name +"</span><a class='save-btn btn-floating halfway-fab waves-effect waves-light red' data-pos=" + [i] + "><i class='material-icons'>♡</i></a></div><div class='card-content'><p>Gender: " + gender + "<br>" + age + "<br>" + description + "<br><a href='" + link + "'>Learn More</a></p></div></div></div></div>")
+        $("#animalResults").append("<div class='col s12 m6'><div class='card'><div class='card-image'><img src="+ srcImage + "><span class='card-title'>" + name +"</span><a class='save-btn btn-floating halfway-fab waves-effect waves-light red' data-pos=" + position + "><i class='material-icons'>♡</i></a></div><div class='card-content'><p>Gender: " + gender + "<br>" + age + "<br>" + description + "<br><a href='" + link + "'>Learn More</a></p></div></div></div></div>")
 
 
       }
@@ -60,7 +66,7 @@ function fetchAnimals(url) {
 
 $("#animalResults").on("click", ".save-btn", function(){
   var animalInfo = animalArray[$(this).attr('data-pos')];
-
+ 
   if (animalInfo.primary_photo_cropped) {
     var srcImage = animalInfo.primary_photo_cropped.small
   } else {
@@ -71,27 +77,53 @@ $("#animalResults").on("click", ".save-btn", function(){
    var description = animalInfo.description;
    var age = animalInfo.age;
    var link = animalInfo.URL;
+   var position = $(this).attr('data-pos');
   // this is gross because it's a materialize card
-  $("#favorite").append("<div class='col s12 m6'><div class='card'><div class='card-image'><img src="+ srcImage + "><span class='card-title'>" + name +"</span><a class='lock-btn btn-floating halfway-fab waves-effect waves-light red' data-pos=><i class='material-icons'>🔒</i></a></div><div class='card-content'><p>Gender: " + gender + "<br>" + age + "<br>" + description + "<br><a href='" + link + "'>Learn More</a></p></div></div></div></div>")
+  $("#favorite").append("<div class='col s12 m6'><div class='card'><div class='card-image'><img src="+ srcImage + "><span class='card-title'>" + name +"</span><a class='lock-btn btn-floating halfway-fab waves-effect waves-light red' data-pos=" + position + "><i class='material-icons'>🔒</i></a></div><div class='card-content'><p>Gender: " + gender + "<br>" + age + "<br>" + description + "<br><a href='" + link + "'>Learn More</a></p></div></div></div></div>")
 
-   favorites.push (animalInfo);
+  favorites.push (animalInfo);
+
   window.localStorage.setItem("favorites", JSON.stringify(favorites));
 
 });
 
-$("#animalResults").on("click", ".lock-btn", function() {
+$("#favorite").on("click", ".lock-btn", function() {
+  var animalInfo = favorites[$(this).attr('data-pos')];
+  console.log(animalInfo);
+  theOne.push(animalInfo);
 
+  window.localStorage.setItem("theOne", JSON.stringify(theOne));
+
+  window.location.assign('names.html')
 });
 
+function checkStorage() {
+  if (favorites) {
+    for (let i = 0; i < favorites.length; i++) {
 
-function search(animaltype, city) {
-  let testURL = 'https://api.petfinder.com/v2/animals?type=' + animaltype + '&location=' + city;
-  fetchAnimals(testURL);
-  }
+      if (favorites[i].primary_photo_cropped) {
+        var srcImage = favorites[i].primary_photo_cropped.small
+      } else {
+        var srcImage = 'https://sanfrancisco.cbslocal.com/wp-content/uploads/sites/15116056/2011/10/pets.jpg?w=420'
+      }
+       var name = favorites[i].name;
+       var gender = favorites[i].gender;
+       var description = favorites[i].description;
+       var age = favorites[i].age;
+       var link = favorites[i].URL;
+       var position = favorites[i].position;
+      // this is gross because it's a materialize card
+      $("#favorite").append("<div class='col s12 m6'><div class='card'><div class='card-image'><img src="+ srcImage + "><span class='card-title'>" + name +"</span><a class='lock-btn btn-floating halfway-fab waves-effect waves-light red' data-pos=" + position + "><i class='material-icons'>🔒</i></a></div><div class='card-content'><p>Gender: " + gender + "<br>" + age + "<br>" + description + "<br><a href='" + link + "'>Learn More</a></p></div></div></div></div>")
+    };
+  };
+};
+
+
   
 
 
 getToken();
+checkStorage ();
 
 
 $(document).ready(function() {
